08502363258cac4303fc017e0a48a836f6e62448
Prathibha Annem <prathibha.annem@slu.edu>
2023-10-27 00:05:34 -0500

    connecting the data base for the job applicant register-1

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+import snowflake.connector
 
 app = Flask(__name__)
 app.secret_key = 'navya'  # Replace with your own secret key
 
 # Mock user data (replace with a database)
 users = []
+
+# Function to create a Snowflake connection
+def create_snowflake_connection():
+    snowflake_config = {
+        'account': 'anohoex-igb93598',
+        'user': 'BEYONDBACKGROUNDS',
+        'password': 'Beyondpswd1',
+        'warehouse': 'COMPUTE_WH',
+        'database': 'BEYONDBACKGROUNDS',
+        'schema': 'SCH_BEYONDBACKGROUNDS',
+        'role': 'ACCOUNTADMIN'
+    }
+
+    try:
+        conn = snowflake.connector.connect(**snowflake_config)
+        return conn
+    except Exception as e:
+        print("Snowflake Connection Error:", str(e))
+        raise e
+
 
 @app.route('/')
 def index():
@@ -27,7 +48,7 @@ def register(user_type):
                 hashed_password = generate_password_hash(password)
                 users.append({'username': username, 'password': hashed_password})
                 flash('Recruiter registration successful.', 'success')
-                return redirect(url_for('index'))
+                return redirect(url_for('user_details'))
 
         return render_template('recruiter_register.html')
 
@@ -45,7 +66,7 @@ def register(user_type):
                 hashed_password = generate_password_hash(password)
                 users.append({'username': username, 'password': hashed_password})
                 flash('Job applicant registration successful.', 'success')
-                return redirect(url_for('index'))
+                return redirect(url_for('user_details'))
 
         return render_template('applicant_register.html')
 
@@ -80,27 +101,40 @@ def about():
 @app.route('/home')
 def home():
     return render_template('home.html')
-@app.route('/register/applicant', methods=['GET', 'POST'])
-def register_applicant():
+@app.route('/register/applicant', methods=['POST'])
+@app.route('/job_postings', methods=['POST'])
+def job_postings():
     if request.method == 'POST':
-        # Registration logic for job applicants
-        username = request.form['username']
-        password = request.form['password']
+        try:
+            # Extract register from the form
+            username = request.form['username']
+            password = request.form['password']
+            
+            # Create a new Snowflake connection
+            conn = create_snowflake_connection()
 
-        # Check if the username is already taken (in a real application, use a database)
-        if any(user['username'] == username for user in users):
-            flash('Username already exists. Please choose another.', 'danger')
-        else:
-            # Store the user data (in a real application, use a database)
-            hashed_password = generate_password_hash(password)
-            users.append({'username': username, 'password': hashed_password})
-            flash('Job applicant registration successful.', 'success')
-            return redirect(url_for('index'))
-    else:
-        flash('Invalid user type.', 'danger')
-        return render_template('applicant_register.html')
+            # Execute an SQL insert statement using the Snowflake connection
+            cursor = conn.cursor()
+            query = """INSERT INTO ApplicantDetails (username, password) VALUES (?, ?)"""
 
+            # Execute the query with parameters
+            cursor.execute(query, (username,password))
+            cursor.close()
 
+            # Commit the transaction
+            conn.commit()
+
+            # Close the Snowflake connection
+            conn.close()
+
+            flash('Registrarion sucessfull', 'success')
+
+        except Exception as e:
+            print(e)
+            app.logger.error(f"An error occurred ")
+            flash('An error occurred. Please try again later.', 'error')
+
+    return redirect(url_for('user_details'))
 @app.route('/logout')
 def logout():
     session.pop('user', None)

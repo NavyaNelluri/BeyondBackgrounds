26b4406da1e7140ebcbb3a11ed07f456c7266888
Josna <josna.pitta@slu.edu>
2023-10-27 19:52:43 -0500

    added comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for
 import snowflake.connector
 
 app = Flask(__name__)
@@ -17,41 +17,43 @@ def create_snowflake_connection():
         'schema': 'SCH_BEYONDBACKGROUNDS',
         'role': 'ACCOUNTADMIN'
     }
-    print("Snowflake Configuration:", snowflake_config)  # Debugging line
+    print("Snowflake Configuration:", snowflake_config)
 
     try:
         conn = snowflake.connector.connect(**snowflake_config)
         return conn
     except Exception as e:
-        print("Snowflake Connection Error:", str(e))  # Debugging line
+        print("Snowflake Connection Error:", str(e))  
         raise e
 
-
+#login page to enter username and password
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         
-        # Check the database for the record based on username and password
+        # Check the database for the username and password
         if check_credentials(username, password):
-            # Successful login
+            #Redirects to Dashboard on successful login
             return redirect(url_for('dashboard'))
         else:
             app.error_message = 'Invalid username or password'
 
     return render_template('login.html', error_message=app.error_message)
 
+#Checks the provided crendentials for authentication
 def check_credentials(username, password):
     try:
-        # Create a new Snowflake connection
+        #Database connection establishment 
         conn = create_snowflake_connection()
-
-        # Execute an SQL query to check the credentials
         cursor = conn.cursor()
+
+        #Execute the query
         query = "SELECT * FROM UserDetails WHERE USERNAME = %s AND PASSWORD = %s"
         cursor.execute(query, (username, password))
 
+        #Fetch the results
         results = cursor.fetchall()
         cursor.close()
 
@@ -63,6 +65,7 @@ def check_credentials(username, password):
         print(e)
         return False
 
+#Redirects to Dashboard
 @app.route('/dashboard')
 def dashboard():
     return 'Welcome to the Beyond Backgrounds'
--- test.py
+++ test.py
@@ -2,19 +2,22 @@ import pytest
 import app
 
 
+#Test case to test the success scenario of login page
 def test_check_credentials_pass():
     username = 'Navya Nelluri'
     password = 'Navya.c@698'
 
+    #calls the function with correct credentials
     result = app.check_credentials(username, password)
 
     assert(result,True)
 
-
+#Test case to test the fail scenario of login page
 def test_check_credentials_fail():
     username = 'ABC'
     password = 'xyz'
 
+    #calls the function with wrong credentials
     result = app.check_credentials(username, password)
 
     assert(result,False)

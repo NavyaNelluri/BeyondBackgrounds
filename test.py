45d1dded3860aca03a286ad0c734ec0a190d6276
josna-pitta <123142263+josna-pitta@users.noreply.github.com>
2023-10-27 21:12:58 -0500

    added authentication testcase

--- test.py
+++ test.py
@@ -39,5 +39,26 @@ def test_username():
 # Running the test case
 test_username()
 
+#Test case to test the success scenario of login page
+def test_check_credentials_pass():
+    username = 'Navya Nelluri'
+    password = 'Navya.c@698'
+
+    #calls the function with correct credentials
+    result = app.check_credentials(username, password)
+
+    assert(result,True)
+
+#Test case to test the fail scenario of login page
+def test_check_credentials_fail():
+    username = 'ABC'
+    password = 'xyz'
+
+    #calls the function with wrong credentials
+    result = app.check_credentials(username, password)
+
+    assert(result,False)
+
+
 if __name__ == "__main__":
     pytest.main()

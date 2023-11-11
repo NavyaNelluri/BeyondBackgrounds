f3fd4508bb954d88b88798644e4b9a9515fdcee5
Prathibha Annem <123604006+Prathibha0@users.noreply.github.com>
2023-11-10 19:50:48 -0600

    Update test.py
    added 2 testcases

--- test.py
+++ test.py
@@ -59,6 +59,15 @@ def test_check_credentials_fail():
     result = app.check_credentials(username, password)
 
     assert(result,False)
+def test_job_portal_page(self):
+        # Ensure that the job portal page returns a 200 status code
+        response = self.app.get('/JobPortal')
+        self.assertEqual(response.status_code, 200)
+
+def test_login_page(self):
+        # Ensure that the login page returns a 100 status code
+        response = self.app.get('/login')
+        self.assertEqual(response.status_code, 100)
 
 
 if __name__ == "__main__":

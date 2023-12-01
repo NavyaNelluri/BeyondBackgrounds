51dfbac5f2fb6a5a5a168dddc2fe5a09af468360
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-11-30 20:39:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -365,5 +365,5 @@ def ApplicantDetailsPage():
     return render_template('ApplicantDetails.html')
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='localhost', port=80)
 
